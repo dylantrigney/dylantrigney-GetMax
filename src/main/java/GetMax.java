e6ededba3ef24487ee62fe73e13079862a3e04be
dylantrigney <dylan.rigney@revature.net>
2023-03-07 17:21:54 +0000

    getmax

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,13 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int max = -100000000;
+
+        for (int num : arr) {
+            if(num > max) {
+                max = num;
+            }
+        }
+        return max;
     }
 }
